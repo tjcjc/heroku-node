a5476119551b0f71275988fef8f675e87ce1d6c3
tjcjc <taijcjc@gmail.com>
2011-09-27 08:12:07 +0000

    lalala

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('lalala ');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.C9_PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
